e52f0e3c02656cfbc0e3651acb61ceb7836a05bc
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-08-10 08:43:41 -0300

    update styles

--- client/src/components/CreateActivity.jsx
+++ client/src/components/CreateActivity.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import {postActivity, getCountries } from '../actions/index';
 import {useDispatch, useSelector} from 'react-redux';
+import World from '../styles/assets/World.svg'
 import '../styles/CreateActivity.css'
  //import axios from 'axios';
 
@@ -90,68 +91,95 @@ export default function CreateActivity() {
 
 
     return (
-        <div className= 'contain'>
-            <div className= 'create'>
-                <Link to='/home'><button>Volver</button></Link>
-                <div>
-                    <h1>Crea la actividad!</h1>
+
+            <div className= 'container-create'>
+                <div className='create-nav'>
+                    <Link to='/home'><button className='create-select'>HOME</button></Link>
+                    <div className='create-title'>
+                        <h1>CREA LA ACTIVIDAD!</h1>
+                    </div>
                 </div>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <div>
-                        <label>Pais: </label>
-                        <select onChange={(e) => handleSelect(e)} className= 'input'>
-                            <option>Elegir pais</option>                                                  
-                            {countries.map((e) => (
-                                <option key={e.name} value={e.name} > {e.name} </option>
-                            ))}
-                        </select>  
-                        {errors.countryId && (
-                            <p className= 'error'>{errors.countryId}</p>
-                        )}
-                        
+                <div className='cont-form-body'>
+                    <div className='f-form'>
+                        <div className='cont-form'>
+                            <form onSubmit={(e) => handleSubmit(e)}>
+                                <h1 className='form-title'>INGRESE LOS DATOS</h1>
+                                <div className='form-country'>
+                                    <div className='form-label'>
+                                        <label>Pais: </label>
+                                    </div>
+                                    <div>
+                                        <select onChange={(e) => handleSelect(e)} className= 'form-select'>
+                                            <option>Elegir pais</option>                                                  
+                                            {countries.map((e) => (
+                                                <option key={e.name} value={e.name} > {e.name} </option>
+                                            ))}
+                                        </select>  
+                                        {errors.countryId && (
+                                            <p className= 'error'>{errors.countryId}</p>
+                                        )}
+                                    </div>
+                                </div>
+                                <div className='cont-section-form'>
+                                    <div className='form-label'>
+                                        <label >Nombre: </label>
+                                    </div>
+                                    <input type= 'text' value={input.name} name= 'name' onChange={(e) => handleChange(e)} className='form-input'/>
+                                    {errors.name && (
+                                        <p className= 'error'>{errors.name}</p>
+                                    )}
+                                </div>
+                                <div className='cont-section-form'>
+                                    <div className='form-label'>
+                                        <label >Dificultad: </label>
+                                    </div>
+                                    <input type='number' value= {input.difficulty} name= 'difficulty' onChange={(e) => handleChange(e)} className= 'form-input'/>
+                                    {errors.difficulty && (
+                                        <p className= 'error'>{errors.difficulty}</p>
+                                    )}
+                                </div>
+                                <div className='cont-section-form'>
+                                    <div className='form-label'>
+                                        <label >Duracion: </label>
+                                    </div>
+                                    <input type= 'text' value= {input.duration} name= 'duration' onChange={(e) => handleChange(e)} className= 'form-input'/>
+                                    {errors.duration && (
+                                        <p className= 'error'>{errors.duration}</p>
+                                    )}
+                                </div>
+                                <div className='form-country'>
+                                    <div className='form-label'>
+                                        <label>Temporada: </label>
+                                    </div>
+                                    <select name= 'season' value= {input.season} onChange={(e) => handleChange(e)} className= 'form-select'>
+                                        <option value="verano">Verano</option>
+                                        <option value= 'otoño' >Otoño</option> 
+                                        <option value= 'invierno'>Invierno</option>  
+                                        <option value= 'primavera'>Primavera</option>                     
+                                    </select>
+                                    {errors.season && (
+                                        <p className= 'error'>{errors.season}</p>
+                                    )}                        
+                                </div>
+                                <div className='form-label'>
+                                    {input.country.map(e => (
+                                        <h3>{e}</h3>
+                                    ))}
+                                </div>
+                                <div className='cont-section-form'>
+                                    <button type='submit' className='createbutton'>CREAR ACTIVIDAD</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div>
-                        <label>Nombre: </label>
-                        <input type= 'text' value={input.name} name= 'name' onChange={(e) => handleChange(e)} className= 'input'/>
-                        {errors.name && (
-                            <p className= 'error'>{errors.name}</p>
-                        )}
+                    <div className='cont-img'>
+                        <img src={World} 
+                        alt='img-notfound'
+                        className='img-create'/>
                     </div>
-                    <div>
-                        <label>Dificultad: </label>
-                        <input type= 'number' value= {input.difficulty} name= 'difficulty' onChange={(e) => handleChange(e)} className= 'input'/>
-                        {errors.difficulty && (
-                            <p className= 'error'>{errors.difficulty}</p>
-                        )}
-                    </div>
-                    <div>
-                        <label>Duracion:  </label>
-                        <input type= 'text' value= {input.duration} name= 'duration' onChange={(e) => handleChange(e)} className= 'input'/>
-                        {errors.duration && (
-                            <p className= 'error'>{errors.duration}</p>
-                        )}
-                    </div>
-                    <div>
-                        <label>Temporada: </label>
-                        <select name= 'season' value= {input.season} onChange={(e) => handleChange(e)} className= 'input'>
-                            <option value="verano">Verano</option>
-                            <option value= 'otoño' >Otoño</option> 
-                            <option value= 'invierno'>Invierno</option>  
-                            <option value= 'primavera'>Primavera</option>                     
-                        </select>
-                        {errors.season && (
-                            <p className= 'error'>{errors.season}</p>
-                        )}                        
-                    </div>
-                    <ul><li>{input.country.map(e => e + '  ')}</li></ul>
-                    <div>
-                        <button type='submit' className= 'createbutton'>Crear actividad!</button>
-                    </div>
-                </form>
+                </div>
             </div>
 
-
-        </div>
     )
 
 
--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -23,51 +23,59 @@ export default function Detail () {
 
     return (
         <div className= 'container'> 
-
             {
                 detailcountry ?
                 <div className= 'detail' >
-                    <div className= 'detail2'>
-                        <div className= 'info' >
-                            <h2>{detailcountry.name}</h2>
+                    <div>
+                        <div className='detail-cont-name'>
+                            <div className='detail-title'>
+                                <h2>{detailcountry.name}</h2>
+                            </div>
+                            <div>
+                                <img src= {detailcountry.flags} alt='flag-not-found'/>
+                            </div>
                         </div>
-                        <div>
-                            <img src= {detailcountry.flags} alt='flag-not-found'/>
-                        </div>
-                        <div className= 'info'>
-                            <h5>CAPITAL: {detailcountry.capital}</h5>
-                        </div>
-                        <div className= 'info'>
-                            <h5>REGION: {detailcountry.region}</h5>
-                        </div>
-                        <div className= 'info'>
-                            <h5>SUBREGION: {detailcountry.subregion}</h5>
-                        </div>
-                        <div className= 'info'>
-                            <h5>AREA: {detailcountry.area} Kms2 </h5>
-                        </div>
-                        <div className= 'info'>
-                            <h5>POBLACION: {detailcountry.population}</h5>
-                        </div>
-                        <div className= 'info'>
-                            <h5>ACTIVIDADES: </h5>
-                            {
-                                detailcountry.exercises?.map(e => {
-                                    return (
-                                        <div key= {e.id}>
-                                            <h5>Nombre de actividad: {e.name}</h5>
-                                            <h5>Dificultad: {e.difficulty}</h5>
-                                            <h5>Duracion: {e.duration}</h5>
-                                            <h5>Temporada: {e.season}</h5>
+                        <div className='cont-info'>
+                            <div className='cont-info-country'>
+                                <div className='info'>
+                                    <h5>CAPITAL: {detailcountry.capital}</h5>
+                                    <h5>HOLA</h5>
+                                </div>
+                                <div className='info'>
+                                    <h5>REGION: {detailcountry.region}</h5>
+                                </div>
+                                <div className='info'>
+                                    <h5>SUBREGION: {detailcountry.subregion}</h5>
+                                </div>
+                                <div className='info'>
+                                    <h5>AREA: {detailcountry.area} Kms2 </h5>
+                                </div>
+                                <div className='info'>
+                                    <h5>POBLACION: {detailcountry.population}</h5>
+                                </div>
+                            </div>
+                            <div className='cont-info-activity'>
+                                <div className= 'info'>
+                                    <h5>ACTIVIDADES: </h5>
+                                    {
+                                        detailcountry.exercises?.map(e => {
+                                            return (
+                                                <div key= {e.id}>
+                                                    <h5>Nombre de actividad: {e.name}</h5>
+                                                    <h5>Dificultad: {e.difficulty}</h5>
+                                                    <h5>Duracion: {e.duration}</h5>
+                                                    <h5>Temporada: {e.season}</h5>
 
-                                        </div>
-                                    ) 
+                                                </div>
+                                            ) 
 
-                                }) 
-                                
-                            }
+                                        }) 
+                                        
+                                    }
+                                </div>
+                            </div>
                         </div>
-                        <div>
+                        <div className='cont-button'>
                             <Link to= '/home' style={{ textDecoration: 'none'}}>
                                 <buttton className='button' >Volver</buttton>
                             </Link>
